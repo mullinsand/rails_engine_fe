c7884db1a63b898d1f5833345dc6cf5ce883ce21
Andrew Mullins <mullinsand15@gmail.com>
2022-10-26 10:10:15 -0600

    build test framework

---
+++ spec/merchants/show_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe 'Merchant Show page' do
+  describe 'as a visitor' do
+    describe 'when I visit the merchant show page' do
+      it 'has a list of all items the merchant sells by name' do
+
+      end
+
+      it 'each items name is a link to their show page' do
+        
+      end
+
+      it 'does not show other merchants items' do
+
+      end
+    end
+  end
+end
